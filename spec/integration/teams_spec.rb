629ffb36e50f972034e6b2cc6ff2fd75e7cb16de
Chalo Fernandez <chalofa@gmail.com>
2012-01-23 22:31:39 -0600

    Integration test

---
+++ spec/integration/teams_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe Nfler::Teams do
+  subject { Nfler::Teams.get }
+  before do
+    Nfler::Page.stub! get: read_test_file('teams_wikipedia.html')
+  end
+
+  describe '#get' do
+    it 'should be an array' do
+      subject.should be_an_instance_of Array
+    end
+
+    it 'should have 32 teams' do
+      pending 'This should work after filtering Teams'
+      subject.size.should == 32
+    end
+
+    it 'should have Team instances inside' do
+      subject.first.should be_an_instance_of Nfler::Team
+    end
+  end
+
+end
