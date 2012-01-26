5d57d83c54e795616717390bdf1a05d6f27d7eed
Chalo Fernandez <chalofa@gmail.com>
2012-01-25 22:18:16 -0600

    Team initial tests

--- lib/nfler/team.rb
+++ lib/nfler/team.rb
@@ -2,12 +2,20 @@ module Nfler
 
   # Each of the Teams in the NFL
   class Team
+    attr_accessor :name, :area, :stadium, :founded, :joined, :coach
+
 
     # Creates a new Team
     #
-    # @param [Hash] data for team (name, area, stadium, founded, joined, head coach, owner)
+    # @param [Hash] data for team (name, area, stadium, founded, joined, coach, owner)
     # @return [Team] a new Team instance
     def initialize(*data)
+    end
+
+
+    # Show a human-readable output of the Team
+    def to_s
+      name
     end
 
   end
--- spec/nfler/team_spec.rb
+++ spec/nfler/team_spec.rb
@@ -1,9 +1,18 @@
 require 'spec_helper'
 
 describe Nfler::Team do
-  describe '#new' do
+
+  describe '#initialize' do
     it 'should create an instance' do
       Nfler::Team.new.should be_instance_of Nfler::Team
     end
   end
+
+  describe '#to_s' do
+    it 'should show a human readable text' do
+      subject.should_receive(:name).and_return 'Team Name'
+      subject.to_s.should == 'Team Name'
+    end
+  end
+
 end
